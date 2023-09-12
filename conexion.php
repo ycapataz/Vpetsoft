7bb4d5e8f388037819d3d3b7517a1db8391b4755
ycapataz <124093827+ycapataz@users.noreply.github.com>
2023-09-12 18:11:37 -0500

    Actualizar conexion.php

--- conexion.php
+++ conexion.php
@@ -6,13 +6,12 @@ $usuario = "root";
 $contrasena = "";
 
 try {
-    // Crear una conexión PDO
+    // Se crea una conexión PDO
     $conexion = new PDO("mysql:host={$host};dbname={$dbname}", $usuario, $contrasena);
 
-    // Establecer el modo de error de PDO a excepciones
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Ahora puedes utilizar $conexion para ejecutar consultas SQL
+
     //echo "La conexión a la base de datos se ha establecido correctamente.";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
