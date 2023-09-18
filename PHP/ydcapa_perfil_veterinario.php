9b61d1f30ed340a44ab648be0c468795e7e9afaf
ycapataz <danielcapataz1@gmail.com>
2023-09-18 18:07:21 -0500

    se agrega a mejoras en el perfil y se logra que al iniciar sesion la persona que inicia sesion pueda ver y editar su informacion

--- PHP/ydcapa_perfil_veterinario.php
+++ PHP/ydcapa_perfil_veterinario.php
@@ -18,6 +18,7 @@ if (!isset($nombre)){
         <link rel="shortcut icon" href="../Imegenes/logo1.png">
         <link rel="stylesheet" href="../CSS/ydcapa_encabezado.css">
         <link rel="stylesheet" href="../CSS/ydcapa_campos_2.css">
+        <link rel="stylesheet" href="../CSS/ydcapa_ventana_emergente.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     </head>
 <body>
@@ -89,7 +90,7 @@ if (!isset($nombre)){
         </header>
         <br><br>
 	<main>
-        <h1><?php echo $nombre; ?></h1>
+        <h1><?php echo $_SESSION['nombre']; ?></h1>
         <div class="img_usuaro">
             <img src="../Imegenes/veterinaria_3.jpg">
         </div>
@@ -99,69 +100,96 @@ if (!isset($nombre)){
             <div class="formulario__grupo" id="Nombre">
 				<label for="Nombre" class="formulario__label">Nombres</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Nombre" id="Nombre" placeholder="Paola Andrea" readonly  value >
+					<input type="text" class="formulario__input" name="Nombre" id="Nombre" placeholder="Paola Andrea" readonly value="<?php echo $_SESSION['nombre'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Apellidos">
 				<label for="Apellidos" class="formulario__label">Apellidos</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Apellidos" id="Apellidos" placeholder="Marleto" readonly  value>
+					<input type="text" class="formulario__input" name="Apellidos" id="Apellidos" placeholder="Marleto" readonly  value="<?php echo $_SESSION['apellido'];?>">
 				</div>
 			</div>
             <!-- Grupo: Cedula -->
 			<div class="formulario__grupo" id="Cedula">
 				<label for="Cedula" class="formulario__label">Numero de Cedula</label>
 				<div class="formulario__grupo-input">
-					<input type="number" class="formulario__input" name="Cedula" id="Cedula" step="1" min="10000000" max="1999999999"placeholder="1026897450" readonly  value>
+					<input type="number" class="formulario__input" name="Cedula" id="Cedula" step="1" min="10000000" max="1999999999"placeholder="1026897450" readonly  value="<?php echo $_SESSION['documento'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Fecha Nacimiento">
 				<label for="Fecha Nacimiento" class="formulario__label">Fecha Nacimiento</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Fecha Nacimiento" id="Fecha Nacimiento" step="1" min="1" max="3" placeholder="06/10/1996" readonly  value>
+					<input type="text" class="formulario__input" name="Fecha Nacimiento" id="Fecha Nacimiento" step="1" min="1" max="3" placeholder="06/10/1996" readonly  value="<?php echo $_SESSION['fecha'];?>">
 				</div>
 			</div>
 			<div class="formulario__grupo" id="Celular">
 				<label for="Celular" class="formulario__label">Numero Celular</label>
 				<div class="formulario__grupo-input">
-					<input type="number" class="formulario__input" name="Celular" id="Celular"min="3002000000" max="3509999999" placeholder="3217864500" readonly  value>
+					<input type="number" class="formulario__input" name="Celular" id="Celular"min="3002000000" max="3509999999" placeholder="3217864500" readonly  value="<?php echo $_SESSION['telefono'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Email">
 				<label for="Email" class="formulario__label">Email</label>
 				<div class="formulario__grupo-input">
-					<input type="email" class="formulario__input" name="Email" id="Email"  placeholder="pamarteloc@dogsano.com" readonly  value >
+					<input type="email" class="formulario__input" name="Email" id="Email"  placeholder="pamarteloc@dogsano.com" readonly  value="<?php echo $_SESSION['email'];?>" >
 				</div>
 			</div>
             <div class="formulario__grupo" id="Direccion">
 				<label for="Direccion" class="formulario__label">Direccion</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Direccion" id="Direccion"  placeholder="Cll 6 # 75 47" readonly  value>
+					<input type="text" class="formulario__input" name="Direccion" id="Direccion"  placeholder="Cll 6 # 75 47" readonly  value="<?php echo $_SESSION['direccion'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Especialidad">
 				<label for="Especialidad" class="formulario__label">Especialidad</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Especialidad" id="Especialidad"  placeholder="Medico general" readonly  value>
+					<input type="text" class="formulario__input" name="Especialidad" id="Especialidad"  placeholder="Medico general" readonly  value="<?php echo $_SESSION['nomespecialidad'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Cargo">
 				<label for="Cargo" class="formulario__label">Cargo</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Cargo" id="Cargo"  placeholder="Veterinario" readonly  value>
+					<input type="text" class="formulario__input" name="Cargo" id="Cargo"  placeholder="Veterinario" readonly  value="<?php echo $_SESSION['nomcargo'];?>">
 				</div>
 			</div>
             <div class="formulario__grupo" id="Eps">
 				<label for="Eps" class="formulario__label">Eps</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="Eps" id="Eps"  placeholder="COMPENSAR S.A." readonly  value>
+					<input type="text" class="formulario__input" name="Eps" id="Eps"  placeholder="COMPENSAR S.A." readonly  value="<?php echo $_SESSION['nomeps'];?>">
 				</div>
 			</div>
             <br><br><br>
 			<div class="formulario__grupo formulario__grupo-btn-enviar">
-				<button type="submit" class="formulario__btn">Editar</button>
+                <button id="btn-abrir-popup" type="button" class="formulario__btn">Editar</button>
 			</div>
 		</form>
 	</main>
+    <div class="overlay" id="overlay">
+			<div class="popup" id="popup">
+				<a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
+				<h4 class="title">Recuerda siempre poner informacion real.</h4>
+				<form action="../PHP/ydcapa_editar_perfil.php" method="POST">
+					<div class="contenedor-inputs">
+                        <input name="telefono" type="number" placeholder="telefono celular" value="<?php echo $_SESSION['telefono'];?>" >
+                        <input name="correo" type="text" placeholder="Correo electronico" value="<?php echo $_SESSION['email'];?>" onfocus="mostrarMensaje()">
+
+                        <div>
+                        <span id="mensaje" style="color: red; position: relative;"></span>
+                        
+                        <script>
+                        function mostrarMensaje() {
+                            var mensajeSpan = document.getElementById('mensaje');
+                            mensajeSpan.textContent = "NOTA: Al editar este campo, editará el correo electrónico con el que inicia sesión.";
+                        }
+                        </script>
+                        </div>
+                        <input name="direccion" type="text" placeholder="Dirección" value="<?php echo $_SESSION['direccion'];?>" >
+					</div>
+					<input type="submit" class="btn-submit" value="editar">
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src="../JAVASCRIPT/ydcapa_ventana.js"></script>
 </body>
 </html>
