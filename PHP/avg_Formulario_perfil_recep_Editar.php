9450d99e3632b85c8c1632493209881f25abe904
AlejandraVGL <lesavelasco@misena.edu.co>
2023-09-21 09:19:46 -0500

    validar sesion funcional

---
+++ PHP/avg_Formulario_perfil_recep_Editar.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include '../conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $correo = $_POST["correo"];
+    $direccion = $_POST["direccion"];
+    $telefono = $_POST["telefono"];
+    $idempleado = $_SESSION['id_empleado'];
+
+    try {
+        $stmt = $conexion->prepare("UPDATE empleado SET corempleado=:corempleado, dirempleado=:dirempleado, telempleado=:telempleado WHERE idempleado=:idempleado");
+        $stmt->bindParam(':corempleado', $correo);
+        $stmt->bindParam(':dirempleado', $direccion);
+        $stmt->bindParam(':telempleado', $telefono);
+        $stmt->bindParam(':idempleado', $idempleado);
+        $stmt->execute();
+        echo '<script>
+        alert("Información actualizada correctamente para ver los cambios cierre sesion e ingresar nuevamente.");
+        window.location.href = "../PHP/avg_Formulario_perfil_recep.php";
+        </script>';
+        echo "";
+    } catch(PDOException $e) {
+        echo "Error al actualizar la información: " . $e->getMessage();
+    }
+}
+?>
